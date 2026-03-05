43af46d366e279355ce73fe412f5028b7d73d37c
Jonathan Burnhams <jburnhams@gmail.com>
2026-03-05 06:41:17 +0000

    Delete patch_integration_tests.sh

--- patch_integration_tests.sh
+++
@@ -1,2 +0,0 @@
-sed -i 's/let cmd = coord.calculate_adjustment();/let cmd = coord.calculate_adjustment_at(now_ptp);/' tests/multi_room_integration.rs
-sed -i 's/let cmd = coord.calculate_adjustment();/let cmd = coord.calculate_adjustment_at(now_ptp);/' src/receiver/ap2/tests/multi_room.rs
